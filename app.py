4c252ba934982769885593b1cb999cf869cbd430
shakill <pshakilwizard@gmail.com>
2022-01-04 18:42:36 +0600

    get url with a post request

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import json
+from flask import jsonify
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/get-photo',  methods=['POST', 'GET'])
+def analysis_photo():
+    print(request.json['url'])
+    data = {
+        'url': request.json['url'],
+        'status': 'ok' 
+    }
+    return jsonify({'data': data}), 201
