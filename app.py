e2e1c65947ccad10c9588d8f8c58f1206cbd47df
anisurrahmann <pshakilwizard@gmail.com>
2022-03-30 18:39:25 +0600

    add image for ml model

--- app.py
+++ app.py
@@ -1,33 +1,62 @@
-from flask import Flask
-import json
-from flask import jsonify
-from flask import request
+import os
+
 import requests
+from flask import Flask, jsonify, request
+from PIL import Image
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "./temp"
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
 
-AIRTABLE_BASE_ID= "appTjCVPvyujwYVh8"
-AIRTABLE_API_KEY= "keysdvFSVMD8PvvPE"
-AIRTABLE_TABLE_NAME= "results-table"
+AIRTABLE_BASE_ID = "appTjCVPvyujwYVh8"
+AIRTABLE_API_KEY = "keysdvFSVMD8PvvPE"
+AIRTABLE_TABLE_NAME = "results-table"
 
-endpoint=f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route('/')
+endpoint = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/get-photo',  methods=['POST', 'GET'])
+
+@app.route("/analyze", methods=["POST", "GET"])
 def analysis_photo():
-    print(request.json['url'])
-    data = {
-        'url': request.json['url'],
-        'status': 'ok' 
-    }
+    if request.method == "POST":
+        file = request.files["photo"]
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.seek(0)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            path = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "temp", filename
+            )
+            # todos
+            # catch other fields data
+            # add to airtable
+            analyzed_results = analyze(path)
+            # add result to airtable
+            os.remove(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-    add_to_airtable("THE_RANDOM_ID", "2/2/22", "fdsafasfdsafdasdfasdfasdfafdasdfasfds")
-    
-    return jsonify({'data': data}), 201
+    return jsonify({"data": "data"}), 201
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def analyze(path):
+    im = Image.open(path)
+    witdth, height = im.size
+
+    print(witdth, height)
+
+    return [("2.5", 0.7), ("1.5", 0.2)]
 
 
 def add_to_airtable(id="", date="", result=""):
@@ -35,18 +64,8 @@ def add_to_airtable(id="", date="", result=""):
         return
     headers = {
         "Authorization": f"Bearer {AIRTABLE_API_KEY}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
-    data = {
-    "records": [
-            {
-            "fields": {
-                "id": id,
-                "date": date,
-                "result": result
-                }
-            }
-        ]
-    }
+    data = {"records": [{"fields": {"id": id, "date": date, "result": result}}]}
     r = requests.post(endpoint, json=data, headers=headers)
     return r.status_code == 200
